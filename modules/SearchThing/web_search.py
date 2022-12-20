0eac7f8c529127482756f03414a3f19109db919c
shitlime <1753102572@qq.com>
2022-12-20 19:47:51 +0800

    修改web_search模块

--- modules/SearchThing/web_search.py
+++ modules/SearchThing/web_search.py
@@ -28,18 +28,24 @@ saya.require("modules.web_search")
 #配置：
 #命令配置：
 #查找参数：
-search_mode_list = ["m", "萌", "萌娘", "萌百", "萌娘百科", "鸡", "小鸡", "小鸡词典", "c", "中国搜索", "百", "百度", "百度搜索", "searXNG", "搜索"]
+search_mode_list = ["m", "萌", "萌娘", "萌百", "萌娘百科",
+"鸡", "小鸡", "小鸡词典",
+"c", "中国搜索",
+"百", "百度", "百度搜索",
+"searXNG", "搜索",
+"bing", "必", "必应", "必应搜索"]
 #查找链接配置：
 url_moegirl = 'https://zh.moegirl.org.cn/index.php?search='
 url_xiaoji = 'https://jikipedia.com/search?phrase='
 url_baidu = 'https://www.baidu.com/s?ie=UTF-8&wd='
 url_chinaso = 'https://www.chinaso.com/newssearch/all/allResults?q='
 url_searxng = 'https://searx.fi/search?q='
+url_bing = 'https://cn.bing.com/search?q='
 
 #消息响应：
 @channel.use(
     ListenerSchema(
-        listening_events=[GroupMessage],
+        listening_events=[GroupMessage, FriendMessage],
         inline_dispatchers=[
             Twilight(
                 UnionMatch(["?", "？"]).space(SpacePolicy.PRESERVE),
@@ -49,7 +55,7 @@ url_searxng = 'https://searx.fi/search?q='
         ]
     )
 )
-async def web_search(app: Ariadne, member: Member, group: Group, message: MessageChain, search_mode: RegexResult, search_string: RegexResult):
+async def web_search(app: Ariadne, member: Member, target: Group | Friend, message: MessageChain, search_mode: RegexResult, search_string: RegexResult):
     search_mode = search_mode.result.display
     search_string = search_string.result.display
     re_msg = None
@@ -68,6 +74,9 @@ async def web_search(app: Ariadne, member: Member, group: Group, message: Messag
     elif search_mode in ["百", "百度", "百度搜索"]:
         search_string = quote(search_string)
         re_msg = MessageChain(Plain(url_baidu + search_string))
+    elif search_mode in ["bing", "必", "必应", "必应搜索"]:
+        search_string = quote(search_string)
+        re_msg = MessageChain(Plain(url_bing + search_string))
     #发送消息
     if re_msg != None:
-        await app.send_message(group, re_msg)
+        await app.send_message(target, re_msg)
