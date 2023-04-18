964e676eee75ff3527abbdbf798226c3b3a1e376
shitlime <1753102572@qq.com>
2023-04-18 16:36:36 +0800

    fix get_60s_news module

--- modules/SixtySecNews/get_60s_news.py
+++ modules/SixtySecNews/get_60s_news.py
@@ -8,19 +8,21 @@ async def get_news_img() -> bytes | int:
     获取60秒看世界的新闻图片
     Returns: bytes | int
     """
-    api_url = 'http://bjb.yunwj.top/php/tp/lj.php'
+    # api_url = 'http://bjb.yunwj.top/php/tp/lj.php'
+    api_url = 'https://api.emoao.com/api/60s'
     async with aiohttp.ClientSession() as session:
         async with session.get(api_url) as result:
             if result.status == 200:
                 data = await result.read()
-                data_json = json.loads(data)
-                img_url = data_json['tp1']
-                async with session.get(img_url) as r:
-                    if r.status == 200:
-                        img = await r.read()
-                        return base64.b64encode(img)
-                    else:
-                        return result.status
+                return base64.b64encode(data)
+                # data_json = json.loads(data)
+                # img_url = data_json['tp1']
+                # async with session.get(img_url) as r:
+                #     if r.status == 200:
+                #         img = await r.read()
+                #         return base64.b64encode(img)
+                #     else:
+                #         return result.status
             else:
                 return result.status
 
@@ -29,4 +31,5 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     b64img = loop.run_until_complete(get_news_img())
     print(b64img)
+    print(len(b64img))
     print(type(b64img))
