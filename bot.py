5abbd3ac3148f28e2477c151dd72730e5b7bb707
shitlime <1753102572@qq.com>
2023-06-01 18:24:53 +0800

    更新：全局消息延时队列，用于躲避风控

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from asyncio.events import AbstractEventLoop
+
 from bot_init import BOT
+from modules.base.check import check_friend
+from modules.base.message_queue import MessageQueue
 
 from graia.saya import Saya
 from graia.ariadne.app import Ariadne
@@ -10,8 +14,8 @@ from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import Plain
 from graia.ariadne.message.parser.base import MentionMe
 from graia.ariadne.model import Friend, Group, Member
-from modules.base.check import check_friend
-from graia.ariadne.util.cooldown import CoolDown
+from graia.ariadne.event.lifecycle import ApplicationLaunched, ApplicationShutdowned
+
 
 saya = Ariadne.create(Saya)
 
@@ -29,7 +33,7 @@ async def friend_message_listener(app: Ariadne, friend: Friend, message: Message
     friendMsg = message.display # type:str
     #print(friend.id) # 打印QQ号码
     if friendMsg == "|say h":
-        await app.send_message(friend,
+        await MessageQueue().send_message(app, friend,
         MessageChain([Plain("Hello, World!")])
         )
 
@@ -49,8 +53,26 @@ async def group_message_listener(app: Ariadne, group: Group, member: Member, mes
         MessageChain([Plain("不在哦")])
         )
 
+# 挂载消息中转
+bg_tsk_1 = None
+@app.broadcast.receiver(ApplicationLaunched)
+async def start_background(loop: AbstractEventLoop):
+    global bg_tsk_1
+    if not bg_tsk_1:
+        bg_tsk_1 = loop.create_task(MessageQueue().send_message_worker())
+
+@app.broadcast.receiver(ApplicationShutdowned)
+async def stop_background():
+    global bg_tsk
+    if bg_tsk:
+        bg_tsk_1.cancel() # 取不取消随你, 但不要留到 Ariadne 生命周期外
+        await bg_tsk_1
+        bg_tsk_1 = None
+
+# 加载saya模块
 with saya.module_context():
     for m in BOT.modules:
         saya.require(m)
 
+# 阻塞式启动
 app.launch_blocking()
---
+++ modules/base/message_queue.py
@@ -0,0 +1,37 @@
+# 发送消息用的中转站
+# 2023-06-01 Shitlime： 为了躲避风控，需要收集全局的消息发送，并进行排队延迟
+
+import asyncio
+
+from graia.ariadne.app import Ariadne
+
+# 全局延时
+msg_delay = 3.5
+
+class MessageQueue:
+    __instance = None
+    
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.queue = asyncio.Queue()
+        return cls.__instance
+    
+    async def send_message_worker(self):
+        print("消息队列开始工作")
+        while True:
+            # 从消息队列中取出待发送的消息
+            app, target, message, quote = await self.queue.get()
+            try:
+                await app.send_message(target, message)
+            except Exception as e:
+               print(f"ERROR: {e}")
+            await asyncio.sleep(msg_delay)
+
+    async def send_message(self, app: Ariadne, target, message, *, quote = False,):
+        # 将待发送的消息添加到队列中
+        print("-----put-----------")
+        await self.queue.put((app, target, message, quote,))
+    
+    # 如果可能，修改昵称
+
